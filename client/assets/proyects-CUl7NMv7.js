053563c1231ef4069f2952766b7a9b98e111487b
Anibalp95 <anibal.palomo.21@gmail.com>
2025-02-24 17:28:31 -0300

    Updates

---
+++ client/assets/proyects-CUl7NMv7.js
@@ -0,0 +1,1 @@
+import{w as t}from"./with-props-Db1jVSdq.js";import{l as o}from"./chunk-HA7DTUK3-DItZKCSJ.js";const s=()=>o.jsx("div",{children:"This is the proyects page"}),p=t(s);export{p as default};
